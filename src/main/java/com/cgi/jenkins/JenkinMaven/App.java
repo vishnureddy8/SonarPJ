35b6226fdeee17e0ff1b44e0a210ee3f307a2707
Vishnu Reddy <findvishnu@gmail.com>
2017-10-27 12:46:07 +0530

    first commit

---
+++ src/main/java/com/cgi/jenkins/JenkinMaven/App.java
@@ -0,0 +1,13 @@
+package com.cgi.jenkins.JenkinMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
